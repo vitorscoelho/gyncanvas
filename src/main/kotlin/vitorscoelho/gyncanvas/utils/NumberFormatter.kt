0e940e49880d0d8984725926c2cf2a12965c8b90
vitorscoelho <vitor.eec@hotmail.com>
2019-10-04 15:54:15 -0300

    Implementada a formatação da medida das Dimensions

---
+++ src/main/kotlin/vitorscoelho/gyncanvas/utils/NumberFormatter.kt
@@ -0,0 +1,36 @@
+package vitorscoelho.gyncanvas.utils
+
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.lang.Math.round
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+class Formatter(
+    suppressLeadingZeros: Boolean,
+    suppressTrailingZeros: Boolean,
+    precision: Int,
+    decimalSeparator: Char,
+    private val prefix: String,
+    private val suffix: String,
+    private val roundoff: Double
+) {
+    private val dc: DecimalFormat
+
+    init {
+        val decimalFormatPatternRight = if (suppressTrailingZeros) "#".repeat(precision) else "0".repeat(precision)
+        val decimalFormatPatternLeft = if (suppressLeadingZeros) "0" else "#"
+        val decimalFormatSymbols = DecimalFormatSymbols()
+        decimalFormatSymbols.decimalSeparator = decimalSeparator
+        val decimalFormatPattern = "$decimalFormatPatternLeft.$decimalFormatPatternRight"
+        this.dc = DecimalFormat(decimalFormatPattern)
+        this.dc.decimalFormatSymbols = decimalFormatSymbols
+    }
+
+    private fun roundoff(valor: Double):Double{
+        if (roundoff==0.0) return valor
+        return roundoff * round(valor.absoluteValue / roundoff) * valor.sign
+    }
+
+    fun format(valor: Double): String = "$prefix${dc.format(roundoff(valor))}$suffix"
+}
---
+++ src/test/kotlin/vitorscoelho/gyncanvas/utils/NumberFormatterTest.kt
@@ -0,0 +1,87 @@
+package vitorscoelho.gyncanvas.utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FormatadorTest {
+    @Test
+    fun formatar1() {
+        val f = Formatter(
+            suppressLeadingZeros = true, suppressTrailingZeros = true, precision = 2, roundoff = 0.25,
+            decimalSeparator = '.', prefix = "valor= ", suffix = "cm"
+        )
+        assertEquals("valor= 5.5cm", f.format(5.5))
+        assertEquals("valor= 5.5cm", f.format(5.54))
+        assertEquals("valor= 5.5cm", f.format(5.6))
+        assertEquals("valor= 5.75cm", f.format(5.65))
+        assertEquals("valor= 5.75cm", f.format(5.71))
+        assertEquals("valor= 5.75cm", f.format(5.75))
+        assertEquals("valor= 5.75cm", f.format(5.77))
+        assertEquals("valor= -5.5cm", f.format(-5.5))
+        assertEquals("valor= -5.5cm", f.format(-5.54))
+        assertEquals("valor= -5.5cm", f.format(-5.6))
+        assertEquals("valor= -5.75cm", f.format(-5.65))
+        assertEquals("valor= -5.75cm", f.format(-5.71))
+        assertEquals("valor= -5.75cm", f.format(-5.75))
+        assertEquals("valor= -5.75cm", f.format(-5.77))
+        assertEquals("valor= 57506.5cm", f.format(57506.5987))
+        assertEquals("valor= 57506.5cm", f.format(57506.5487))
+        assertEquals("valor= 0.5cm", f.format(0.5))
+        assertEquals("valor= -0.5cm", f.format(-0.5))
+        assertEquals("valor= 0cm", f.format(0.0))
+    }
+
+    @Test
+    fun formatar2() {
+        val f = Formatter(
+            suppressLeadingZeros = false, suppressTrailingZeros = false, precision = 3, roundoff = 0.1,
+            decimalSeparator = '.', prefix = "valor= ", suffix = "cm"
+        )
+        assertEquals("valor= 5.500cm", f.format(5.5))
+        assertEquals("valor= 5.500cm", f.format(5.54))
+        assertEquals("valor= 5.600cm", f.format(5.6))
+        assertEquals("valor= 5.700cm", f.format(5.65))
+        assertEquals("valor= 5.700cm", f.format(5.71))
+        assertEquals("valor= 5.800cm", f.format(5.75))
+        assertEquals("valor= 5.800cm", f.format(5.77))
+        assertEquals("valor= -5.500cm", f.format(-5.5))
+        assertEquals("valor= -5.500cm", f.format(-5.54))
+        assertEquals("valor= -5.600cm", f.format(-5.6))
+        assertEquals("valor= -5.700cm", f.format(-5.65))
+        assertEquals("valor= -5.700cm", f.format(-5.71))
+        assertEquals("valor= -5.800cm", f.format(-5.75))
+        assertEquals("valor= -5.800cm", f.format(-5.77))
+        assertEquals("valor= 57506.600cm", f.format(57506.5987))
+        assertEquals("valor= 57506.500cm", f.format(57506.5487))
+        assertEquals("valor= .500cm", f.format(0.5))
+        assertEquals("valor= -.500cm", f.format(-0.5))
+        assertEquals("valor= .000cm", f.format(0.0))
+    }
+
+    @Test
+    fun formatar3() {
+        val f = Formatter(
+            suppressLeadingZeros = true, suppressTrailingZeros = true, precision = 2, roundoff = 0.0,
+            decimalSeparator = '.', prefix = "valor= ", suffix = "cm"
+        )
+        assertEquals("valor= 5.5cm", f.format(5.5))
+        assertEquals("valor= 5.54cm", f.format(5.54))
+        assertEquals("valor= 5.6cm", f.format(5.6))
+        assertEquals("valor= 5.65cm", f.format(5.65))
+        assertEquals("valor= 5.71cm", f.format(5.71))
+        assertEquals("valor= 5.75cm", f.format(5.75))
+        assertEquals("valor= 5.77cm", f.format(5.77))
+        assertEquals("valor= -5.5cm", f.format(-5.5))
+        assertEquals("valor= -5.54cm", f.format(-5.54))
+        assertEquals("valor= -5.6cm", f.format(-5.6))
+        assertEquals("valor= -5.65cm", f.format(-5.65))
+        assertEquals("valor= -5.71cm", f.format(-5.71))
+        assertEquals("valor= -5.75cm", f.format(-5.75))
+        assertEquals("valor= -5.77cm", f.format(-5.77))
+        assertEquals("valor= 57506.6cm", f.format(57506.5987))
+        assertEquals("valor= 57506.55cm", f.format(57506.5487))
+        assertEquals("valor= 0.5cm", f.format(0.5))
+        assertEquals("valor= -0.5cm", f.format(-0.5))
+        assertEquals("valor= 0cm", f.format(0.0))
+    }
+}
